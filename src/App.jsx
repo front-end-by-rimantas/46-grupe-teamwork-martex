da80018aef2bc906863227cf51fd6b3491e9b842
G-Just <gjustas00@gmail.com>
2023-11-14 23:11:36 +0200

    Changing js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import { Hero } from "./components/main/hero/Hero";
+import { Header } from "./components/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
