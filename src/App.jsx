428e75c2936657fbc38a4de477224b383e590e3b
Laima <pileliene.laima@gmail.com>
2023-11-15 01:06:22 +0200

    icons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Hero } from "./components/main/hero/Hero";
 import { Header } from "./components/header/Header";
+import { ServiceItem } from "./components/main/ServiceItem";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <ServiceItem />
     </>
   );
 }
---
+++ src/components/main/ServiceItem.jsx
@@ -0,0 +1,44 @@
+import style from "./ServiceItem.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faLineChart } from "@fortawesome/free-solid-svg-icons";
+import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
+import { faChartColumn } from "@fortawesome/free-solid-svg-icons";
+import { faServer } from "@fortawesome/free-solid-svg-icons";
+
+
+
+
+
+
+export function ServiceItem() {
+    return (
+        <div className={style.servicesContainer}>
+            <div className={style.servicesItem}>
+
+                <div className={style.iconBox} >  <FontAwesomeIcon className={style.icon} icon={faLineChart} /></div>
+
+                <h3> Market Research</h3>
+                <p>Luctus augue egestas undo ultrice and quisque lacus</p>
+            </div>
+            <div className={style.servicesItem}>
+                <div className={style.iconBox} >  <FontAwesomeIcon className={style.icon} icon={faLightbulb} /></div>
+                <h3> Market Research</h3>
+                <p>Luctus augue egestas undo ultrice and quisque lacus</p>
+            </div>
+            <div className={style.servicesItem}>
+                <div className={style.iconBox} >  <FontAwesomeIcon className={style.icon} icon={faChartColumn} /></div>
+                <h3> Market Research</h3>
+                <p>Luctus augue egestas undo ultrice and quisque lacus</p>
+            </div>
+            <div className={style.servicesItem}>
+                <div className={style.iconBox} >  <FontAwesomeIcon className={style.icon} icon={faServer} /></div>
+                <h3> Market Research</h3>
+                <p>Luctus augue egestas undo ultrice and quisque lacus</p>
+            </div>
+
+
+        </div>
+
+    );
+}
