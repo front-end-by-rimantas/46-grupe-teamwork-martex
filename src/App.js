517809bda502a524accdeda39b744279569f444f
Laima <pileliene.laima@gmail.com>
2023-11-14 22:58:35 +0200

    single card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Hero } from "./components/main/hero/Hero";
+import { MarketingServices } from "./components/main/MarketingServices";
+
 
 function App() {
   return (
     <>
       <Hero />
+      <MarketingServices />
     </>
   );
 }
-
 export default App;
