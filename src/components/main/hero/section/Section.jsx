3c0e3b1c617b2cf9fd3fdf6fd8f68dafaa58d7bf
Bartas Dausynas <bartas3@gmail.com>
2023-11-15 13:42:57 +0200

    changes

--- src/components/main/hero/section/Section.jsx
+++ src/components/main/hero/section/Section.jsx
@@ -1,8 +1,8 @@
 import style from "./Block.module.css"
 
-export function Block(){
+export function Section(){
     return (
-        <div className={style.block}>
+        <div className={style.section}>
             <img className={style.img} src="./img/img-10.png" alt="somePhoto" />
             <div className={style.forText}>
                 PLACEHOLDER
