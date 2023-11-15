fede47a05c549e3592c9301dc05a25c761513da7
Bartas Dausynas <bartas3@gmail.com>
2023-11-15 13:51:38 +0200

    changes8

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import "./App.css";
 import { Hero } from "./components/main/hero/Hero";
 import { Header } from "./components/header/Header";
-import { SectionHeader } from "./components/main/section/SectionHeader";
-import { Section } from "./components/main/section/Section";
+
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      <SectionHeader />
-      <Section />
     </>
   );
 }
