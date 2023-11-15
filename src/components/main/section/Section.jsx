9db7ce34715b429ce892f059119f43664ff7174d
Kasparas <kasparas.dapsys@gmail.com>
2023-11-15 14:03:51 +0200

    upd

--- src/components/main/section/Section.jsx
+++ src/components/main/section/Section.jsx
@@ -1,3 +1,4 @@
+import { ExplanatoryText } from "./ExplanatoryText";
 import style from "./Section.module.css"
 
 export function Section(){
@@ -5,7 +6,7 @@ export function Section(){
         <div className={style.section}>
             <img className={style.img} src="./img/img-10.png" alt="somePhoto" />
             <div className={style.forText}>
-                PLACEHOLDER
+                <ExplanatoryText/>
             </div>
         </div>
     );
