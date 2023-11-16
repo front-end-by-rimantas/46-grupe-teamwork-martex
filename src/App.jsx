3e8e8cf0756fabd3a482837c4ef444b8a2232f9c
G-Just <gjustas00@gmail.com>
2023-11-16 15:45:52 +0200

    Wiping project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import "./App.css";
-import { Hero } from "./components/main/hero/Hero";
-import { Header } from "./components/header/Header";
-import { ServiceCard } from "./components/main/section/ServiceCard";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <ServiceCard />
-    </>
-  );
+  return;
 }
 
 export default App;
