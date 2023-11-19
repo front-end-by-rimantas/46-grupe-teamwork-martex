6de3b2380fb68d649b60e232bce0528931104271
Kasparas <kasparas.dapsys@gmail.com>
2023-11-19 20:14:09 +0200

    bootstrap import, class names

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import "bootstrap/dist/css/bootstrap.min.css"
 import  Button  from "react-bootstrap/Button";
 
 
@@ -11,19 +10,19 @@ function App() {
     <>
     <Container>
 
-      <Container className="text-center">
+      <Container>
       <h2>Reach your audience through social media marketing</h2>
       <p>Ligula risus auctor tempus magna feugiat lacinia.</p>
       </Container>
 
-        <Container className="text-center">
+        <Container>
           <Row>
-            <Col s={12} md={6}>
+            <Col className="col-12 col-sm-12 col-md-6">
               <h2>Marketing Integrations</h2>
               <p>Aliquam a augue suscipit luctus diam neque purus ipsum neque and dolor primis libero</p>
               IMG
             </Col>
-            <Col s={12} md={6}>
+            <Col className="col-12 col-sm-12 col-md-6">
               IMG
               <h2>Enhance Engagement</h2>
               <p>Aliquam a augue suscipit luctus diam neque purus ipsum neque and dolor primis libero</p>
@@ -31,37 +30,37 @@ function App() {
           </Row>
         </Container>
 
-        <Container className="text-center">
+        <Container>
         <Row>
 
-          <Col  s={12} md={4} >
+          <Col className="col-12 col-sm-12 col-md-4">
             <Row className="align-items-center">
-              <Col lg={4} md={12} >
+              <Col className="col-12 col-md-12 col-lg-4">
                 89k
               </Col>
-              <Col lg={8} md={12} >
+              <Col className="col-12 col-md-12 col-lg-8">
                 Porta justo integer and velna vitae auctor
               </Col>
             </Row>
           </Col>
 
-          <Col s={12} md={4}>
+          <Col className="col-12 col-sm-12 col-md-4">
             <Row className="align-items-center">
-              <Col lg={4} md={12} >
+              <Col className="col-12 col-md-12 col-lg-4" >
                 76%
               </Col>
-              <Col lg={8} md={12} >
+              <Col className="col-12 col-md-12 col-lg-8" >
                 Ligula magna suscipit vitae and rutrum
               </Col>
             </Row>
           </Col>
 
-          <Col s={12} md={4}>
+          <Col className="col-12 col-sm-12 col-md-4">
             <Row className="align-items-center">
-              <Col lg={4} md={12} >
+              <Col className="col-12 col-md-12 col-lg-4" >
                 4.93
               </Col>
-              <Col lg={8} md={12} >
+              <Col className="col-12 col-md-12 col-lg-8" >
               Sagittis congue augue egestas an egestas
               </Col>
             </Row>
@@ -72,10 +71,10 @@ function App() {
 
         <Container>
           <Row>
-            <Col s={12} md={6}>
+            <Col className="col-12 col-sm-12 col-md-6">
               IMG
             </Col>
-            <Col s={12} md={6}>
+            <Col className="col-12 col-sm-12 col-md-6">
               <h2>The smarter way to work</h2>
               <p>Sodales tempor sapien quaerat ipsum undo congue laoreet turpis neque auctor turpis vitae dolor luctus placerat magna and ligula cursus purus vitae purus an ipsum suscipit</p>
               <h2>Full access to all features</h2>
@@ -90,7 +89,7 @@ function App() {
 
         <Container>
           <Row>
-            <Col s={12} md={6}>
+            <Col className="col-12 col-sm-12 col-md-6">
               <p>ONE-STOP SOLUTION</p>
               <h2>Smart solutions, real-time results</h2>
               <p>Sodales tempor sapien quaerat ipsum and congue undo laoreet turpis neque auctor turpis vitae dolor luctus placerat magna ligula and cursus vitae</p>
@@ -101,26 +100,26 @@ function App() {
               </ul>
             </Col>
 
-            <Col s={12} md={6}>
+            <Col className="col-12 col-sm-12 col-md-6">
             <Row>
-              <Col md={6} s={12}>
+              <Col className="col-12 col-sm-12 col-md-6">
                 IMG
                 <h4>Content Marketing</h4>
                 <p>Porta semper lacus and cursus feugiat at primis ultrice a ligula auctor</p>
               </Col>
-              <Col md={6} s={12}>
+              <Col className="col-12 col-sm-12 col-md-6">
                 IMG
                 <h4>Pay Per Click (PPC)</h4>
                 <p>Porta semper lacus and cursus feugiat at primis ultrice a ligula auctor</p>
               </Col>
             </Row>
             <Row>
-              <Col md={6} s={12}>
+              <Col className="col-12 col-sm-12 col-md-6">
                 IMG
                 <h4>Video Marketing</h4>
                 <p>Porta semper lacus and cursus feugiat at primis ultrice a ligula auctor</p>
               </Col>
-              <Col md={6} s={12}>
+              <Col className="col-12 col-sm-12 col-md-6">
                 IMG
                 <h4>Business Analytics</h4>
                 <p>Porta semper lacus and cursus feugiat at primis ultrice a ligula auctor</p>
@@ -133,10 +132,10 @@ function App() {
 
         <Container>
           <Row>
-            <Col md={6} s={12}>
+            <Col className="col-12 col-sm-12 col-md-6">
               IMG
             </Col>
-            <Col md={6} s={12}>
+            <Col className="col-12 col-sm-12 col-md-6">
               <p>EASY INTEGRATION</p>
               <h2>Plug your essential tools in few clicks</h2>
               <ul>
