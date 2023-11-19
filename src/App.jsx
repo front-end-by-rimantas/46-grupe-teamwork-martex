b820a964af79a256bf9f7bb5b2ca267098a32c83
Bartas Dausynas <bartas3@gmail.com>
2023-11-19 21:20:16 +0200

    repaired layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,28 @@ import Col from 'react-bootstrap/Col';
 function App() {
   return (
     <Container>
-      <Row className="Row">
+      <Row className="block">
         <Col className="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-8 col-xxl-7"> HEADER TEXT </Col>
       </Row>
-      <Row className="Row">
+      <Row className="block">
         <Col className="col-11 col-sm-11 col-md-12 col-lg-12 col-xl-12 col-xxl-12"> BLOCK CONTENT </Col>
       </Row>
-      <Row className="Row">
+      <Row className="block">
         <Col> SOME SLIDING CARDS - tikrinau visur uzima 12col ploti... </Col>
       </Row>
-      <Row className="Row">
+      <Row className="block">
         <Col className="col-11 col-sm-10 col-md-7 col-lg-7 col-xl-7 col-xxl-6"> HEADER TEXT </Col>
       </Row>
-      <Row className="Row">
-        <Col className="col-8 col-sm-8 col-md-11 col-lg-11 col-xl-11 col-xxl-12"> SOME 3 CARDS </Col>
+      <Row className="block">
+        <Col className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4"> CARD </Col>
+        <Col className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4"> CARD </Col>
+        <Col className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4"> CARD </Col>
       </Row>
-      <Row className="Row">
-        <Col className="col-11 col-sm-11 col-md-12 col-lg-12 col-xl-12 col-xxl-12"> BLOCK CONTENT </Col>
+      <Row className="block">
+        <Col className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6"> TEXT </Col>
+        <Col className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6"> IMAGE </Col>
       </Row>
+      
     </Container>
   );
 }
