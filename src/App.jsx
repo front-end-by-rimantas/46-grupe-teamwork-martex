fed70bff1d3aa359a8908447ccae9489caa8a256
KarolinaMonkeviciute <145319640+KarolinaMonkeviciute@users.noreply.github.com>
2023-11-20 13:17:13 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap/Container";
+import { Row } from "react-bootstrap/Row";
+import { Col } from "react-bootstrap/Col";
 
 function App() {
   return (
