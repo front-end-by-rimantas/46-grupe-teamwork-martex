8b57a1061f079f8cedfc33ec7e6d8b298a631d32
Bartas Dausynas <bartas3@gmail.com>
2023-11-20 13:34:54 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,26 @@ import Col from 'react-bootstrap/Col';
 function App() {
   return (
     <Container>
-      <Row className="block">
-        <Col className="col-10 col-sm-10 col-md-8 col-lg-8 col-xl-8 col-xxl-7"> HEADER TEXT </Col>
+      <Row className="justify-content-center">
+        <Col sm={10} md={8} xxl={7} > HEADER TEXT </Col>
+      </Row >
+      <Row className="justify-content-center">
+        <Col sm={11} md={12} > BLOCK CONTENT </Col>
       </Row>
-      <Row className="block">
-        <Col className="col-11 col-sm-11 col-md-12 col-lg-12 col-xl-12 col-xxl-12"> BLOCK CONTENT </Col>
+      <Row className="justify-content-center">
+        <Col> SOME SLIDING CARDS </Col>
       </Row>
-      <Row className="block">
-        <Col> SOME SLIDING CARDS - tikrinau visur uzima 12col ploti... </Col>
+      <Row className="justify-content-center" >
+        <Col sm={10} md={7} xxl={6}> HEADER TEXT </Col>
       </Row>
-      <Row className="block">
-        <Col className="col-11 col-sm-10 col-md-7 col-lg-7 col-xl-7 col-xxl-6"> HEADER TEXT </Col>
+      <Row className="justify-content-center">
+        <Col sm={12} md={4}> CARD </Col>
+        <Col sm={12} md={4}> CARD </Col>
+        <Col sm={12} md={4}> CARD </Col>
       </Row>
-      <Row className="block">
-        <Col className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4"> CARD </Col>
-        <Col className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4"> CARD </Col>
-        <Col className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4"> CARD </Col>
-      </Row>
-      <Row className="block">
-        <Col className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6"> TEXT </Col>
-        <Col className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6"> IMAGE </Col>
+      <Row className="justify-content-center">
+        <Col sm={12} md={6}>TEXT</Col>
+        <Col sm={12} md={6}> IMAGE </Col>
       </Row>
       
     </Container>
