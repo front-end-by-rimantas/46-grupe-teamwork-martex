c4a17102755a603bbe8d3bc5684a1897674973e1
KarolinaMonkeviciute <145319640+KarolinaMonkeviciute@users.noreply.github.com>
2023-11-20 13:16:07 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ function App() {
       </header>
       <Container>
         <Row>
-          <Col className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+          <Col xs={12} sm={6}>
             <h1>Content is the key to building an audience</h1>
             <p>TEXT</p>
             <button>BUTTON</button>
             <p>TEXT</p>
           </Col>
-          <Col className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+          <Col xs={12} sm={6}>
             <div>IMAGE</div>
           </Col>
         </Row>
@@ -33,26 +33,26 @@ function App() {
           </Col>
         </Row>
         <Row>
-          <Col className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+          <Col xs={12} md={6} lg={3}>
             1 of 4
           </Col>
-          <Col className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+          <Col xs={12} md={6} lg={3}>
             2 of 4
           </Col>
-          <Col className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+          <Col xs={12} md={6} lg={3}>
             3 of 4
           </Col>
-          <Col className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
+          <Col xs={12} md={6} lg={3}>
             4 of 4
           </Col>
         </Row>
       </Container>
       <Container>
         <Row>
-          <Col className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+          <Col xs={12} md={6}>
             IMAGE
           </Col>
-          <Col className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+          <Col xs={12} md={6}>
             <p>TEXT</p>
             <h1>Engage your most valuable visitors</h1>
             <p>TEXT</p>
@@ -66,7 +66,7 @@ function App() {
       </Container>
       <Container>
         <Row>
-          <Col className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+          <Col xs={12} md={6}>
             <h3>Solution that grows with you</h3>
             <p>TEXT</p>
             <h3>Connect your data sources</h3>
@@ -76,7 +76,7 @@ function App() {
               <li>TEXT</li>
             </ul>
           </Col>
-          <Col className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+          <Col xs={12} md={6}>
             IMAGE
           </Col>
         </Row>
